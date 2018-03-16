8e20ce1ee6c41f3c0ea9425cd967fcf27d8ec4c0
Alex Panas <alexpanasucla@gmail.com>
2018-03-17 01:22:48 +0300

    add files to cache.addAll

--- sw.js
+++ sw.js
@@ -1,11 +1,36 @@
-// pre-cashing static files on installation of service worker
+// Caching static files with service worker
 
 self.addEventListener('install',(event)=>{
   event.waitUntil(
     caches.open('static')
       .then((cache)=>{
-        cache.add('/js/main.js');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/restaurant.html',
+          '/js/dbhelper.js',
+          '/js/main.js',
+          '/js/restaurant_info.js',
+          '/data/restaurants.json',
+          '/css/styles.css',
+          '/css/responsive.css',
+          '/css/responsive_restaurants.css',
+          '/restaurant.html?id=2'
+        ]);
       })
-  )
+    );
+});
 
+
+self.addEventListener('fetch',(event)=>{
+  event.respondWith(
+    caches.match(event.request)
+      .then((response)=>{
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request);
+    }
+  })
+);
 });
