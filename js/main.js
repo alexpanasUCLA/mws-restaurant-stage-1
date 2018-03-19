fe6d22c7dea0156624a7cf62115c8870392b3e80
Alex Panas <alexpanasucla@gmail.com>
2018-03-19 22:00:09 +0300

    change text in alt on main page

--- js/main.js
+++ js/main.js
@@ -140,7 +140,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = `image of ${restaurant.name}`;
+  image.alt = `image of restaurant ${restaurant.name}`;
   li.append(image);
 
   const name = document.createElement('h1');
